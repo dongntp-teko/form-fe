5bf32e73ca245f7aa21cedfd704c63a2ce0dae61
dongntp-teko <dong.ntp@teko.vn>
2019-07-10 17:55:26 +0700

    first

---
+++ src/components/SigninForm/SigninForm.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { withFormik, Form } from 'formik'
+import * as Yup from 'yup'
+
+function SignupForm(props){
+    document.title = `Login`;
+    return(
+        <Router>
+        <div className="border-form">
+            <div className="m20 text-center">
+                <h2>Register</h2>
+                <hr />
+                <Form>
+                    <div className='form-group input-group'>
+                        <div className='input-group-prepend'>
+                            <span className='input-group-text'><i class="fas fa-user"></i></span>
+                        </div>
+                        <input className='form-control' name='username' value={props.values.username} placeholder="Username" onChange={props.handleChange} />
+                    
+                    </div>
+                    <div>
+                        <p className='err'>{props.errors.username}</p>
+                    </div>
+                    <div className='form-group input-group'>
+                        <div className='input-group-prepend'>
+                            <span className='input-group-text'><i class="fas fa-lock"></i></span>
+                        </div>
+                        <input type='password' className='form-control' name='password1' value={props.values.password1} placeholder="Create password" onChange={props.handleChange} />
+                    </div>
+
+                    <div className='form-group'>
+                        <Link to="/" type='submit' className='btn btn-primary btn-block'>Sign up</Link>
+                    </div>
+                </Form>
+            </div>
+    
+        </div>
+        </Router>
+     
+    )
+}
+
+const FormikForm = withFormik({
+    mapPropsToValues() { 
+        return {
+            username: '',
+            email: '',
+            password1: '',
+            password2: '',
+        }
+    },
+    validationSchema: Yup.object().shape({
+        username: Yup.string()
+            .required('Username is required')
+            .min(5, 'Username must have min 5 characters')
+            .max(50, 'Username have max 50 characters'),
+        email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid'),
+        password1: Yup.string()
+            .required('Password is required')
+            .min(6, 'Password must have min 6 characters')
+    })
+})(SignupForm)
+
+export default FormikForm
